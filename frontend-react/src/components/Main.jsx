4eac463c1253c2939330d7fce1e61747de340700
Paulo Barbosa <pmmrb1967@gmail.com>
2024-10-23 15:42:06 +0100

    home page setup

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+          <h1 className='text-light'>Stock Prediction Portal</h1>
+          <p className='text-light lead'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <Button text="Login" class="btn-outline-info" />
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default Main
