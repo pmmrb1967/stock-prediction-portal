60753f34d36382ff94139ee63d38e7cec83ce9ba
Paulo Barbosa <pmmrb1967@gmail.com>
2024-11-07 17:24:38 +0000

    integrating ML model

--- backend-drf/stock_prediction_main/settings.py
+++ backend-drf/stock_prediction_main/settings.py
@@ -145,3 +145,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+# Media files Configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
